46ab423fcd6c1edce4564aff1d30d8e19ece0607
angerhang <514140809@qq.com>
2016-10-07 21:52:05 -0400

    Add clean data

--- scripts/cleanData.R
+++ scripts/cleanData.R
@@ -1,1 +1,10 @@
-# get rid of the 0 quantity entries
+data <- read.csv('../data/rawData.csv')
+attach(data)
+
+## normalizae day 
+data$DAY <- DAY - min(DAY)
+
+## remove quantity with value 0
+data <- data[QUANTITY != 0 ,]
+
+write.csv(data, '../data/cleanData.csv')
